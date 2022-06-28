257e2cae7a13b80a500002d27a4ae9f6010a4381
Ryan Barnes <ryan.barnes@atlanticbt.com>
2022-06-28 17:17:59 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,65 @@
+<div class="container mx-auto">
+	<!-- Navr -->
+	<nav class="p-6 flex flex-row justify-between">
+		<img src="logo.svg" alt="logo" />
+		<img src="icon-hamburger.svg" alt="menu" />
+	</nav>
+	<!-- Hero -->
+	<div class="bg-[url('bg-intro-mobile.svg')] bg-cover">
+		<img src="image-mockups.png" alt="mockup image" class="" />
+	</div>
+	<div class="p-6 flex flex-col gap-4 text-center items-center">
+		<h1 class="text-4xl">Next generation digital banking</h1>
+		<p class="text-grayBlue">
+			Take your financial life online. Your Easybank account will be a one-stop-shop for
+			spending, saving, budgeting, investing, and much more.
+		</p>
+		<button class="rounded-full py-3 bg-gradient-to-r from-limeGreen to-brightCyan w-1/2"
+			>Request Invite</button
+		>
+	</div>
+	<div class="p-6 flex flex-col gap-4 text-center items-center">
+		<h1 class="text-4xl">Why choose Easybank?</h1>
+
+		<p class="text-grayBlue">
+			We leverage Open Banking to turn your bank account into your financial hub. Control your
+			finances like never before.
+		</p>
+	</div>
+	<!-- icon cards -->
+	<div class=" p-6 flex flex-col text-center gap-6 bg-veryLightGray">
+		<div class="flex flex-col items-center gap-6">
+			<img src="icon-online.svg" alt="online icon" />
+			<h2 class="text-2xl ">Online Banking</h2>
+			<p class="text-grayBlue">
+				Our modern web and mobile applications allow you to keep track of your finances
+				wherever you are in the world
+			</p>
+		</div>
+		<div class="flex flex-col items-center gap-6">
+			<img src="icon-budgeting.svg" alt="budgeting icon" />
+			<h2 class="text-2xl ">Simple Budgeting</h2>
+			<p class="text-grayBlue">
+				Our modern web and mobile applications allow you to keep track of your finances
+				wherever you are in the world
+			</p>
+		</div>
+		<div class="flex flex-col items-center gap-6">
+			<img src="icon-onboarding.svg" alt="onboarding icon" />
+			<h2 class="text-2xl ">Fast Onboarding</h2>
+			<p class="text-grayBlue">
+				Our modern web and mobile applications allow you to keep track of your finances
+				wherever you are in the world
+			</p>
+		</div>
+		<div class="flex flex-col items-center gap-6">
+			<img src="icon-api.svg" alt="api icon" />
+			<h2 class="text-2xl ">Open API</h2>
+			<p class="text-grayBlue">
+				Our modern web and mobile applications allow you to keep track of your finances
+				wherever you are in the world
+			</p>
+		</div>
+
+	</div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme'); const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		colors: {
+			darkBlue: 'hsl(233, 26%, 24%)',
+			limeGreen: 'hsl(136, 65%, 51%)',
+			brightCyan: 'hsl(192, 70%, 51%)',
+			grayBlue: 'hsl(233, 8%, 62%)',
+			lightGrayBlue: 'hsl(220, 16%, 96%)',
+			veryLightGray: 'hsl(0, 0%, 98%)',
+			white: 'hsl(0,0%,0%)'
+		},
+		fontFamily: {
+			'sans': ['Public Sans', ...defaultTheme.fontFamily.sans],
+		},
+		extend: {}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
