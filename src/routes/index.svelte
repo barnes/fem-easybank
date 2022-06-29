0e8ea68334148f8c3597c59ac7c5a2e4c28197ae
Ryan Barnes <rybarnes@gmail.com>
2022-06-28 20:37:28 -0400

    small progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,29 @@
-<div class="container mx-auto">
+<div class="mx-12 mx-auto">
 	<!-- Navr -->
 	<nav class="p-6 flex flex-row justify-between">
 		<img src="logo.svg" alt="logo" />
 		<img src="icon-hamburger.svg" alt="menu" />
 	</nav>
 	<!-- Hero -->
-	<div class="bg-[url('bg-intro-mobile.svg')] bg-cover">
+	<div
+		class="md:flex md:flex-row md:flex-row-reverse md:items-center md:bg-contain bg-no-repeat md:bg-right bg-[url('bg-intro-mobile.svg')] bg-cover"
+	>
 		<img src="image-mockups.png" alt="mockup image" class="" />
+		<div class="p-6 flex flex-col gap-4 text-center items-center">
+			<h1 class="text-4xl">Next generation digital banking</h1>
+			<p class="text-grayBlue">
+				Take your financial life online. Your Easybank account will be a one-stop-shop for spending,
+				saving, budgeting, investing, and much more.
+			</p>
+			<button class="rounded-full py-3 bg-gradient-to-r from-limeGreen to-brightCyan w-1/2"
+				>Request Invite</button
+			>
+		</div>
 	</div>
-	<div class="p-6 flex flex-col gap-4 text-center items-center">
-		<h1 class="text-4xl">Next generation digital banking</h1>
-		<p class="text-grayBlue">
-			Take your financial life online. Your Easybank account will be a one-stop-shop for
-			spending, saving, budgeting, investing, and much more.
-		</p>
-		<button class="rounded-full py-3 bg-gradient-to-r from-limeGreen to-brightCyan w-1/2"
-			>Request Invite</button
-		>
-	</div>
-	<div class="p-6 flex flex-col gap-4 text-center items-center">
+
+	<div
+		class="py-6 flex flex-col gap-4 text-center items-center md:items-start md:text-right bg-veryLightGray"
+	>
 		<h1 class="text-4xl">Why choose Easybank?</h1>
 
 		<p class="text-grayBlue">
@@ -27,39 +32,38 @@
 		</p>
 	</div>
 	<!-- icon cards -->
-	<div class=" p-6 flex flex-col text-center gap-6 bg-veryLightGray">
+	<div class=" px-6 flex flex-col text-center gap-6 bg-veryLightGray md:grid md:grid-cols-4">
 		<div class="flex flex-col items-center gap-6">
 			<img src="icon-online.svg" alt="online icon" />
 			<h2 class="text-2xl ">Online Banking</h2>
 			<p class="text-grayBlue">
-				Our modern web and mobile applications allow you to keep track of your finances
-				wherever you are in the world
+				Our modern web and mobile applications allow you to keep track of your finances wherever you
+				are in the world
 			</p>
 		</div>
 		<div class="flex flex-col items-center gap-6">
 			<img src="icon-budgeting.svg" alt="budgeting icon" />
 			<h2 class="text-2xl ">Simple Budgeting</h2>
 			<p class="text-grayBlue">
-				Our modern web and mobile applications allow you to keep track of your finances
-				wherever you are in the world
+				Our modern web and mobile applications allow you to keep track of your finances wherever you
+				are in the world
 			</p>
 		</div>
 		<div class="flex flex-col items-center gap-6">
 			<img src="icon-onboarding.svg" alt="onboarding icon" />
 			<h2 class="text-2xl ">Fast Onboarding</h2>
 			<p class="text-grayBlue">
-				Our modern web and mobile applications allow you to keep track of your finances
-				wherever you are in the world
+				Our modern web and mobile applications allow you to keep track of your finances wherever you
+				are in the world
 			</p>
 		</div>
 		<div class="flex flex-col items-center gap-6">
 			<img src="icon-api.svg" alt="api icon" />
 			<h2 class="text-2xl ">Open API</h2>
 			<p class="text-grayBlue">
-				Our modern web and mobile applications allow you to keep track of your finances
-				wherever you are in the world
+				Our modern web and mobile applications allow you to keep track of your finances wherever you
+				are in the world
 			</p>
 		</div>
-
 	</div>
 </div>
